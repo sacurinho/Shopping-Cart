a9cb3e47b1dd609e614c2c04bdc8a142e49f5b58
Alardo Sacur Junior <145338639+sacurinho@users.noreply.github.com>
2025-09-27 22:19:14 +0200

    Update view.js

--- client/carrinho-js/view.js
+++ client/carrinho-js/view.js
@@ -1,17 +1,20 @@
 function renderCarrinho(produto) {
+    // Calcular initial line total for display
+    const precoTotal = (parseFloat(produto.preco.replace("MZN", "").trim()) * parseFloat(produto.qt)).toFixed(2)
+
     return `
         <div class="cart-item">
             <div class="item-details">
                 <p class="produto-nome">${produto.nome}</p>
                 <div class="item-prices">
                     <span class="price-new">
-                        ${produto.preco}
+                        MZN ${precoTotal}
                     </span>
                 </div>
             </div>
             <div class="item-controls">
                 <div class="quantity-control">
-                    <input type="number" class="quantity-input" value="1" min="1">
+                    <input type="number" class="quantity-input" value="${produto.qt}" min="1" onClick="alterarQt(this)"/>
                 </div>
                 <button class="remove-item">
                     <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="trash-alt" class="svg-inline--fa fa-trash-alt fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
